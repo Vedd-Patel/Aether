479cedd3e9e1f715d6fc37aa8aec91e02f3946e7
Ved Patel <24bcs188@iiitdmj.ac.in>
2026-03-01 00:39:11 +0530

    Initial Commit

---
+++ Aether/AetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AetherApp.swift
+//  Aether
+//
+//  Created by VED PATEL on 01/03/26.
+//
+
+import SwiftUI
+
+@main
+struct AetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
